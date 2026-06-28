83cfdb9f1cc6f367e1ff9f41cb232ad8fac0cff4
Patryk Mikitka <67163@student.wsiz.edu.pl>
2026-06-28 18:22:51 +0200

    .

--- Pharmacy/Controllers/MedicationController.cs
+++ Pharmacy/Controllers/MedicationController.cs
@@ -1,4 +1,5 @@
 using Microsoft.AspNetCore.Mvc;
+using Pharmacy.Database;
 
 namespace Pharmacy.Controllers;
 
@@ -6,15 +7,22 @@ namespace Pharmacy.Controllers;
 [Route("[controller]")]
 public class MedicationController : ControllerBase
 {
+    private readonly PharmacyDbContext _context;
+
+    public MedicationController(PharmacyDbContext context)
+    {
+        _context = context;
+    }
+
     [HttpGet]
     public IActionResult Get()
     {
-        return Ok(MedicationData.Medications);
+        return Ok(_context.Medication);
     }
 
     [HttpGet("{id}")]
     public IActionResult GetById(int id)
     {
-        return Ok(MedicationData.Medications.Where(x => x.Id == id));
+        return Ok(_context.Medication.Where(x => x.Id == id));
     }
 }
--- Pharmacy/Program.cs
+++ Pharmacy/Program.cs
@@ -1,3 +1,6 @@
+using Microsoft.EntityFrameworkCore;
+using Pharmacy.Database;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +9,19 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddDbContext<PharmacyDbContext>(options => options.UseSqlite("Data Source=Database//Pharmacy.db"));
+
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowAngular",
+        policy =>
+        {
+            policy.WithOrigins("http://localhost:4200")
+                  .AllowAnyHeader()
+                  .AllowAnyMethod();
+        });
+});
 
 var app = builder.Build();
 
@@ -17,6 +33,8 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+
+app.UseCors("AllowAngular");
 
 app.UseAuthorization();
 
